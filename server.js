e565277b042f8bc5c348d591e57c065329356b16
tyfnclk <tyfn57@hotmail.com>
2022-01-25 10:43:19 +0300

    Tyfn1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var port = 3000;
 var app = express();
 var wikiUrls1 = "";
-var ConflictVar2 = "Tayfun Çelik";
+var TestConflict = "NurSareCelik";
 //test
 //pull requestchanging
 rp(url)
